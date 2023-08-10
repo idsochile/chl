e91e74d55ad3290baec79c327a3cfd8605fdbeb7
Vanessa Pizarro <va.pizarropizarro@gmail.com>
2023-08-10 15:13:49 -0400

    actualización CW

--- prep/CW/CW.R
+++ prep/CW/CW.R
@@ -1,14 +1,23 @@
-
 library(readxl)
 library(dplyr)
 library(tidyr)
 library(reshape2)
 
 
-trash_pressure <- read_excel("prep/CW/trash_pressure.xlsx")
+data <- read_excel("prep/CW/Marine_nutrient_pollution.xlsx")
+
+
+data[,3]<- rescale_max(data[,3])
+data[,4] <- rescale_max(data[,4])
+data[,5]<- rescale_max(data[,5])
+data[,6] <- rescale_max(data[,6])
+data[,7]<- rescale_max(data[,7])
+
+data<- data[,-2]
+
 
 #Ordenar la tabla en formato largo
-pres<-melt(cw_nutrientsmar, id.vars = c("rgn_id"))
+pres<-melt(data, id.vars = c("rgn_id"))
 
 
 pres<- rename(pres, year = "variable")
@@ -17,15 +26,21 @@ pres$year<- as.numeric(pres$year)
 
 #PO
 pres<- rename(pres, pressure_score = "value")
-pres_po<- pres %>% filter(year == 2021)
-write.csv(cs, "comunas/layers/cs_seaweed_pat2021.csv", row.names = F)
+pres_po<- pres %>%
+  filter(year == 2021) %>%
+  select(rgn_id, pressure_score)
+
+pres_po <- pres_po[!is.na(pres_po$pressure_score),]
+
+write.csv(pres_po, "comunas/layers/cw_connutrientesmar_chl2023.csv", row.names = F)
 
 #Tendencia
+pres <- pres[!is.na(pres$pressure_score),]
 pres_trend <- data.frame()
-for (i in c(1:36)) {
+for (i in unique(pres$rgn_id)) {
   t<- pres %>% filter(rgn_id == i)
-  mdl = lm(value ~ year, data = t)
-  trend =  coef(mdl)[['year']] * sd(pres$year) / sd(pres$value)
+  mdl = lm(pressure_score ~ year, data = t)
+  trend =  coef(mdl)[['year']] * sd(pres$year) / sd(pres$pressure_score)
   sector_trend = pmax(-1, pmin(1, trend * 5))
   d<- data.frame(halpern.trend = sector_trend, coef.Beta0 = coef(mdl)[['(Intercept)']],  coef.year = coef(mdl)[['year']])
   trend = data.frame(rgn_id = i, trend = d$halpern.trend)
@@ -36,7 +51,10 @@ pres_trend<- cbind(pres_trend, "year" = c(rep(2021, nrow(pres_trend))))
 pres_trend<- select(pres_trend, rgn_id, year, trend)
 
 
-write.csv(pres_trend, "comunas/layers/cw_nutrient_trend_pat2021.csv", row.names = F)
+write.csv(pres_trend, "comunas/layers/cw_connutrientesmar_trend_chl2021.csv", row.names = F)
+
+
+##########
 
 
 prs = c('po_pathogen' = 'a',
